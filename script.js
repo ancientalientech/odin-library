c71323d68340736a2dc49966756f73a99e347cb9
ancientalientech <ancientalientech@proton.me>
2025-07-27 18:29:18 +0200

    Display the library on the page

--- script.js
+++ script.js
@@ -24,3 +24,35 @@ const addBookToLibrary = (title, author, pages, read) => {
 addBookToLibrary("Fish", "The Dude", 256, true);
 addBookToLibrary("Meat", "The Guy", 128, false);
 addBookToLibrary("Potato", "The Man", 380, true);
+
+const container = document.querySelector(".table");
+
+const showLibrary = () => {
+    const tempBody = document.createElement("tbody");
+    myLibrary.forEach(book => {
+        const tempRow = document.createElement("tr");
+
+        const titleElement = document.createElement("td");
+        titleElement.textContent = book.title;
+
+        const authorElement = document.createElement("td");
+        authorElement.textContent = book.author;
+
+        const pagesElement = document.createElement("td");
+        pagesElement.textContent = book.pages;
+
+        const readElement = document.createElement("td");
+        readElement.textContent = book.read ? "Read" : "Not read yet";
+
+        tempRow.appendChild(titleElement);
+        tempRow.appendChild(authorElement);
+        tempRow.appendChild(pagesElement);
+        tempRow.appendChild(readElement);
+
+        tempBody.appendChild(tempRow);
+    });
+    container.appendChild(tempBody);
+}
+
+showLibrary();
+
